c8203bd65976e4df61d8be745507485cfc2aa913
Paweł Prorok <pawel3474@tlen.pl>
2019-11-28 09:50:49 +0100

    Finish logic

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 let playerMove = 'papier';
 
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2){
+    computerMove = 'papier';
+} else (randomNumber == 3){
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
